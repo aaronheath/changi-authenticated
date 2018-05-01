05bdc5a6ff9dc16522250b38ecb179f0df08ed9f
Aaron Heath <aaron@aaronheath.com>
2018-05-02 04:14:12 +0930

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {AuthService} from "./auth.service";
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
+import {AuthGuard} from './auth.guard';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path: '', component: HomeComponent, canActivate: [AuthGuard]},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    RouterModule.forRoot(routes, {enableTracing: true}),
   ],
   providers: [
     AuthService,
