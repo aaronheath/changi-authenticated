46938756360cd1881ad90bcd15975375435a439d
Aaron Heath <aaron.heath@staff.nuksope.com.au>
2018-05-02 15:54:02 +0930

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {AuthService} from "./auth.service";
@@ -9,27 +8,5 @@ import {AuthService} from "./auth.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  loginForm: FormGroup;
-
-  constructor(private auth: AuthService, private fb: FormBuilder) {
-    this.auth.authenticated.subscribe(authenticated => this.isAuthenticated = authenticated);
-
-    this.createForm();
-  }
-
-  title = 'app';
-  isAuthenticated;
-
-  createForm() {
-    this.loginForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-    });
-  }
-
-  submit() {
-    console.log('submitting', this.loginForm.value)
-
-    this.auth.authenticate(this.loginForm.value.username, this.loginForm.value.password);
-  }
+  constructor() {}
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  isAuthenticated;
 
-  constructor() { }
+  constructor(private auth: AuthService, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.auth.authenticated.subscribe(authenticated => this.isAuthenticated = authenticated);
+
+    this.createForm();
   }
 
+  createForm() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  submit() {
+    console.log('submitting', this.loginForm.value)
+
+    this.auth.authenticate(this.loginForm.value.username, this.loginForm.value.password);
+  }
 }
