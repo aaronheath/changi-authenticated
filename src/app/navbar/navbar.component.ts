c71cf98a021b46e9dc69f3f1e8ee97526c21cbc9
Aaron Heath <aaron@aaronheath.com>
2018-05-11 15:50:40 +0930

    wip

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  showSubNav = false;
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  mouseenter(a, b) {
+    console.log('mouseenter', a, b)
+    this.showSubNav = true;
+  }
+
+  mouseleave(a, b) {
+    console.log('mouseleave', a, b)
+    this.showSubNav = false;
+  }
 }
